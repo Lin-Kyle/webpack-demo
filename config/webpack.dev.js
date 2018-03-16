dff95b1195aedc398cd679063814faf00b66e744
linyk <linyk@yunyichina.cn>
2018-03-16 19:18:37 +0800

    test

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,24 +1,23 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
         mode: "development",
         devtool: 'inline-source-map',
+        entry: {
+                app: [/*'webpack-dev-server/client?http://0.0.0.0:9000', 'react-hot-loader/patch', */'./src/index.js']
+        },
         devServer: {
                 contentBase: path.resolve(__dirname, 'dist'),
                 hot: true,
                 port: 9000
         },
         output: {
-                filename: '[name].[hash].js',
+                // filename: '[name].[hash:8].js',
+                filename: '[name].bundle.js',
         },
-        plugins: [
-                new webpack.NamedModulesPlugin(),
-                new webpack.HotModuleReplacementPlugin()
-        ],
+        plugins: [new webpack.NamedModulesPlugin(), new webpack.HotModuleReplacementPlugin()]
 })
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,18 +1,27 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
         mode: "production",
         devtool: 'source-map',
+        entry: {
+                app: ['./src/index.js'],
+        },
         output: {
-                filename: '[name].[chunkhash].js',
+                filename: '[name].[chunkhash:8].js'
         },
         plugins: [
-                new CleanWebpackPlugin(['dist']),
+                new CleanWebpackPlugin(['dist/*.*'], {
+                        root: __dirname,
+                        verbose: true,
+                        dry: false
+                }),
                 new HtmlWebpackPlugin({template: 'index.html'}),
-                new webpack.HashedModuleIdsPlugin(),
+                new webpack.HashedModuleIdsPlugin()
         ]
 })
